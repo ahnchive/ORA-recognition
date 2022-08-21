d70e2bc2c04141bd44437d474e44989c9ea47ac3
--local <ahnchive@gmail.com>
2022-08-21 14:26:12 +0000

    model performance

--- rrcapsnet_original.py
+++ rrcapsnet_original.py
@@ -1024,7 +1024,7 @@ def compute_hypothesis_based_acc(objcaps_len_step_narrow, y_hot, only_acc=True):
     first_zero_index = get_first_zero_index(pdiff)
     final_pred= torch.gather(pstep, 1, first_zero_index).flatten()
     accs = torch.eq(final_pred.to(y_hot.device), y_hot.max(dim=1)[1]).float()
-    nstep = (first_zero_index.flatten()+1).cpu().numpy()
+    nstep = (first_zero_index.flatten()+1) #.cpu().numpy()
     if only_acc:
         return accs
     else:
@@ -1032,7 +1032,7 @@ def compute_hypothesis_based_acc(objcaps_len_step_narrow, y_hot, only_acc=True):
 
 
 
-def compute_entropy_based_acc(objcaps_len_step_narrow, y_hot, threshold=1.0, use_cumulative = True, only_acc= True):
+def compute_entropy_based_acc(objcaps_len_step_narrow, y_hot, threshold=0.6, use_cumulative = False, only_acc= True):
     from torch.distributions import Categorical
 
     def get_first_true_index(boolarray, axis=1, when_no_true='final_index'):
@@ -1069,7 +1069,7 @@ def compute_entropy_based_acc(objcaps_len_step_narrow, y_hot, threshold=1.0, use
 
     final_pred = torch.gather(pred, dim=1, index= first_true_index).flatten()
     accs = torch.eq(final_pred.to(y_hot.device), y_hot.max(dim=1)[1]).float()
-    nstep = (first_true_index.flatten()+1).cpu().numpy()
+    nstep = (first_true_index.flatten()+1) #.cpu().numpy()
 
     if only_acc:
         return accs
@@ -1078,8 +1078,9 @@ def compute_entropy_based_acc(objcaps_len_step_narrow, y_hot, threshold=1.0, use
     
 def acc_fn(objcaps_len_step, y_true, acc_type= 'entropy', single_step=None):
     '''
-    1) topk accuracy: format should be top@k,
-    2) 
+    1) entropy
+    2) hypothesis
+    3) topk accuracy: format should be top@k,
     '''
     num_classes = y_true.size(dim=1) #onehot vector
     objcaps_len_step_narrow = objcaps_len_step.narrow(dim=-1,start=0, length=num_classes) # in case a background cap was added    
@@ -1105,82 +1106,6 @@ def acc_fn(objcaps_len_step, y_true, acc_type= 'entropy', single_step=None):
         
     else:
         raise NotImplementedError('given acc functions are not implemented yet')
-
-#     if 'top' in acc_type:
-#         # get final prediction
-#         y_pred = objcaps_len_step_narrow[:,-1]
-#         topk = int(acc_type.split('@')[1])
-#         accs = topkacc(y_pred, y_true, topk=topk)
-        
-#     elif acc_type =='hypothesis':
-#         # hypothesis testing 2 consecutive --> correct
-#         if single_step:
-#             # get final prediction
-#             y_pred = objcaps_len_step_narrow[:,-1]
-#             accs = topkacc(y_pred, y_true, topk=1)
-#         else:
-
-#             def get_first_zero_index(x, axis=1):
-#                 cond = (x == 0)
-#                 return ((cond.cumsum(axis) == 1) & cond).max(axis, keepdim=True)[1]
-
-#             pstep = objcaps_len_step_narrow.max(dim=-1)[1]
-#             pnow = pstep[:,1:]
-#             pbefore = pstep[:,:-1]
-
-#             pdiff = (pnow-pbefore)
-#             null_column = -99*torch.ones(pdiff.size(0),1).to(pdiff.device)
-#             pdiff = torch.cat([null_column, pdiff], dim=1) # first step as null
-#             pdiff[:,-1]=0 # final step as zero
-#             nstep = get_first_zero_index(pdiff)
-#             y_pred= torch.gather(pstep, 1, nstep).flatten()
-#             accs = torch.eq(y_pred, y_true.max(dim=1)[1]).float()
-        
-#     elif acc_type == 'entropy'
-#         from torch.distributions import Categorical
-
-#         def get_first_true_index(boolarray, axis=1, when_no_true='final_index'):
-#             # boolarray = Batch x Stepsize
-#             first_true_index = ((boolarray.cumsum(axis) == 1) & boolarray).max(axis, keepdim=True)[1] # when no true, set as 0
-
-#             if when_no_true == 'final_index': # when there is no true, use final index
-#                 final_index = boolarray.shape[1]-1
-#                 no_true_condition = (~boolarray).all(dim=1).reshape(-1,1)
-#                 first_true_index = first_true_index + final_index * no_true_condition
-#                 return  first_true_index, no_true_condition
-#             else:
-#                 return first_true_index
-
-#         if use_cumulative:
-#             score = objcaps_len_step_narrow.cumsum(dim=1)
-#             pred = score.max(dim=-1)[1]
-#         else:
-#             score = objcaps_len_step_narrow # Batch x Stepsize x Category
-#             pred = score.max(dim=-1)[1] # Batch x Stepsize
-
-
-#         # compute entropy from softmax output with Temp scale
-#         T=0.2
-#         softmax = F.softmax(score/T, dim=-1) # torch.Size([1000, 4, 10])
-#         entropy = Categorical(probs = softmax).entropy() # torch.Size([1000, 4])
-
-#         # entropy thresholding
-#         stop = entropy<threshold
-#         boolarray = (stop == True)
-
-#         # get first index that reached threshold
-#         first_true_index, no_stop_condition = get_first_true_index(boolarray, axis=1, when_no_true='final_index')
-
-#         final_pred = torch.gather(pred, dim=1, index= first_true_index).flatten()
-#         accs = torch.eq(final_pred.cpu(), y_hot.max(dim=1)[1])
-#         nstep = (first_true_index.flatten()+1).cpu().numpy()
-
-#         return nstep, final_pred, acc, no_stop_condition
-
-#     nstep, pred_model, acc_model_check, no_stop_condition = get_nstep(objcaps_len_step_narrow, y_hot, threshold=1.0, use_cumulative = False)
-    
-#     else:
-#         raise NotImplementedError('given acc functions are not implemented yet')
         
     return accs
 
